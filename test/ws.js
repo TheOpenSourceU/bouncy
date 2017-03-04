2ed49e35214be6a0d823641011bca2779f77402b
Frank Villasenor <frank.villasenor@gmail.com>
2017-03-03 22:44:34 -0600

    Exploring ways to fix dependency and unit tests issues...  This check in fixes the dependencies so they install but the tests fail.

--- test/ws.js
+++ test/ws.js
@@ -1,6 +1,6 @@
 var test = require('tap').test;
 var bouncy = require('../');
-var ws = require('websocket-server');
+var WebSocket = require('ws');
 var wc = require('wsclient');
 
 test('ws', function (t) {
@@ -10,9 +10,10 @@ test('ws', function (t) {
         s1.close();
         setTimeout(process.exit, 100); // whatever
     });
-    
+
     var p0 = Math.floor(Math.random() * (Math.pow(2,16) - 1e4) + 1e4);
-    var s0 = ws.createServer();
+    //var s0 = ws.createServer();
+    var s0 = new WebSocket.Server({port: p0}, connect);
     s0.on('connection', function (c) {
         var msgs = [ 'beepity', 'boop' ];
         c.on('message', function (msg) {
@@ -21,9 +22,12 @@ test('ws', function (t) {
             if (msgs.length === 0) c.close();
         });
     });
-    
-    s0.listen(p0, connect);
-    
+    s0.on('error', function(er) {
+        console.log('WebSocket', er);
+    });
+
+    //s0.listen(p0, connect);
+
     var p1 = Math.floor(Math.random() * (Math.pow(2,16) - 1e4) + 1e4);
     var s1 = bouncy(function (req, bounce) {
         bounce(p0);
@@ -32,10 +36,10 @@ test('ws', function (t) {
     
     var connected = 0;
     function connect () {
-        if (++connected !== 2) return;
+        if (++connected !== 2) return true;
         
-        var c = wc.websocket('ws://localhost:' + p1 + '/');
-        c.connect();
+        var c = wc.websocket('ws://localhost:' + p1 + '/'); //create a websocket to bouncy
+
         
         c.on('open', function () {
             c.send('beepity');
@@ -43,7 +47,9 @@ test('ws', function (t) {
                 c.send('boop');
             }, 15);
         });
-        
+
+        c.connect();
+
         var msgs = [ 'ytipeeb', 'poob' ];
         c.on('message', function (buf) {
             t.equal(buf.toString(), msgs.shift());
